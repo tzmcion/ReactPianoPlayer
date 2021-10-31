dc876db022deee6a96ff116d7f844d0775920008
unknown <tymsonekjelonek@gmail.com>
2021-10-31 13:15:52 +0100

    esthetic change

--- src/Components/PlayingManagement/PlayingManagement.tsx
+++ src/Components/PlayingManagement/PlayingManagement.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import './PlayingManagement.scss';
 
 import MidiPlayer from '../../Helpers/MidiPlayer';
+import LogoPrototype from '../../Assets/piano_icon.png'
 
 interface PlayingManagementProps{
     Player: MidiPlayer,
@@ -18,7 +19,7 @@ export default function PlayingManagement({Player,onEvent}:PlayingManagementProp
             something && clearTimeout(something);
             something = setTimeout(()=>{
                 setOpacity(0);
-            },3000);
+            },2000);
             setOpacity(1)
         });
     },[])
@@ -44,10 +45,12 @@ export default function PlayingManagement({Player,onEvent}:PlayingManagementProp
     return (
         <div className='Playing_main' style={{opacity:opacity}}>
             <div className='icons'>
-            <i className="fa fa-play-circle-o" aria-hidden="true" onClick={handleClick}></i>
-            <i className="fa fa-pause" aria-hidden="true" onClick={handlePause}></i>
-            <i className="fa fa-stop" aria-hidden="true" onClick={handleStop}></i>
+            <img src={LogoPrototype} alt='Logo' onClick={()=>{window.location.reload()}} title='Go back ?' className='IconGoBack LogoImg' />
+            <i className="fa fa-play-circle-o" aria-hidden="true" onClick={handleClick} title='Start Playing'></i>
+            <i className="fa fa-pause" aria-hidden="true" onClick={handlePause} title='Pause/Unpause'></i>
+            <i className="fa fa-stop" aria-hidden="true" onClick={handleStop} title='Reset'></i>
             </div>
+            <h3 className='Timer'>{Math.floor(Player.timer / 1000)}/{Math.floor(Player.MidiLength / 1000)} (seconds)</h3>
             <div className='Duration'>
                 <div className='Bar' style={{width: (Player.timer / Player.MidiLength * 100).toString() + '%'}} />
             </div>

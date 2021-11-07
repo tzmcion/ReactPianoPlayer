14193f6f85a71bfdb35b58d7c97b6c8a2db882b8
unknown <tymsonekjelonek@gmail.com>
2021-11-07 15:06:34 +0100

    poprawka

--- src/Helpers/CanvasEffects/index.ts
+++ src/Helpers/CanvasEffects/index.ts
@@ -1,14 +1,17 @@
 import DancingLines from "./DancingLines";
 
 const KeyGradient = (ctx:CanvasRenderingContext2D,pos_x:number,block_width:number,height:number) =>{
-    const radialGradient = ctx.createRadialGradient(pos_x + block_width/2, height, 0, pos_x + block_width/2, height, 50);
+    const radius:number = 50;
+    const radialGradient = ctx.createRadialGradient(pos_x + block_width/2, height, 0, pos_x + block_width/2, height, radius);
     radialGradient.addColorStop(0.0, 'rgba(200,150,100,1)');
-    radialGradient.addColorStop(0.4, 'rgba(200,150,100,0.2)');
+    radialGradient.addColorStop(0.2, 'rgba(200,150,100,0.7)');
+    radialGradient.addColorStop(0.4, 'rgba(200,150,100,0.4)');
+    radialGradient.addColorStop(0.5, 'rgba(200,150,100,0.1)');
     radialGradient.addColorStop(0.90, 'rgba(200,150,100,0.02)');
     radialGradient.addColorStop(1, 'transparent');
     ctx.fillStyle = radialGradient;
     ctx.beginPath();
-    ctx.arc(pos_x + block_width/2, height, 50, 0, 2 * Math.PI);
+    ctx.arc(pos_x + block_width/2, height, radius, 0, 2 * Math.PI);
     ctx.fill();
 }
 

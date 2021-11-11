5f7bfa9cbcca20c3470791c8e2723911c417ee06
unknown <tymsonekjelonek@gmail.com>
2021-11-11 21:07:16 +0100

    adasd

--- src/Utils/Default.ts
+++ src/Utils/Default.ts
@@ -1,5 +1,5 @@
 import { Options } from "./TypesForOptions";
 
-const DefaultOptions:Options = {Color:'#6DC7AB',OctaveLines:true,KeyPressColor:'#11d331',RandomColors:false,IsEffects:false, backgroundImage: '',speed:35, playSpeed:10, watermark:true,soundOn:true,renderMethod:'animationFrame'}
+const DefaultOptions:Options = {Color:'#6DC7AB',OctaveLines:true,KeyPressColor:'#11d331',RandomColors:false,IsEffects:false, backgroundImage: '',speed:35, playSpeed:10, watermark:true,soundOn:true,renderMethod:'Interval'}
 
 export {DefaultOptions};
